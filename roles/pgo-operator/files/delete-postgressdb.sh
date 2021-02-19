02fbf3c0b42af76522c98064b5e2dc6d2540b779
Tosin Akinosho <takinosh@redhat.com>
2021-02-19 16:19:27 -0500

    fixing root user issue

--- roles/pgo-operator/files/delete-postgressdb.sh
+++ roles/pgo-operator/files/delete-postgressdb.sh
@@ -2,11 +2,11 @@
 
 set -x
 export PGO_OPERATOR_NAMESPACE="${1}"
-export PATH=/root/.pgo/${1}:$PATH
-export PGOUSER=/root/.pgo/${1}/pgouser
-export PGO_CA_CERT=/root/.pgo/${1}/client.crt
-export PGO_CLIENT_CERT=/root/.pgo/${1}/client.crt
-export PGO_CLIENT_KEY=/root/.pgo/${1}/client.key
+export PATH=${3}/.pgo/${1}:$PATH
+export PGOUSER=${3}/.pgo/${1}/pgouser
+export PGO_CA_CERT=${3}/.pgo/${1}/client.crt
+export PGO_CLIENT_CERT=${3}/.pgo/${1}/client.crt
+export PGO_CLIENT_KEY=${3}/.pgo/${1}/client.key
 export PGO_NAMESPACE=${1}
 
 export PGO_APISERVER_URL="https://$(${2} -n "${1}" get route postgres-operator -o jsonpath="{.spec.host}")"
